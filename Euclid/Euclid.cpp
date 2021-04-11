3940c39ab39a88f7efd04bb66232c1e3379c15d1
github <kitakazetoshirokuma@gmail.com>
2021-04-11 21:23:45 +0900

    euclid-program

---
+++ Euclid/Euclid.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+using namespace std;
+
+int main() {
+  int a,b;
+  scanf("%d", &a);
+  scanf("%d", &b);
+
+  while(a!=b) {
+    if(a > b) {
+      a-=b;
+    } else {
+      b -= a;
+    }
+  }
+
+  printf("%d\n", a);
+  return 0;
+}
