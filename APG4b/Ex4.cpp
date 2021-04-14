1264855b0ba5e65f8f844cbacef69bb0e8d0cd5d
github <kitakazetoshirokuma@gmail.com>
2021-04-14 23:09:44 +0900

    Ex4

---
+++ APG4b/Ex4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int seconds = 365 * 24 * 60 * 60;
+ 
+  // 以下のコメント/* */を消して追記する
+  cout << seconds /* 1年は何秒か */ << endl;
+  cout << 2 * seconds /* 2年は何秒か */ << endl;
+  cout << 5 * seconds /* 5年は何秒か */ << endl;
+  cout << 10 * seconds /* 10年は何秒か */ << endl;
+  return 0;
+}
