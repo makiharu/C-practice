520275288608e193e28a129a62ca9ed2d05c7b77
github <kitakazetoshirokuma@gmail.com>
2021-04-17 00:19:05 +0900

    practice-c1

--- APG4b/tempCodeRunnerFile.cpp
+++ APG4b/tempCodeRunnerFile.cpp
@@ -1,22 +1,4 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  int p, price, N;
-  string text;
-  cin >> p;
-
-  // パターン1
-  if (p == 1) {
-    cin >> price;
-    cout << price * N << endl;
-  } else if(p == 2) {
-    cin >> text >> price;
-    cin >> N;
-    cout << text << "!" << endl;
-    cout << price * N << endl;
-  } else {
-  }
-
-  
-}
+  // while(i < B) {
+  //   cout << "B:" << "]" << endl;
+  //   i++;
+  // }
