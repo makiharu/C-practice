3a49209eec9eb05caadd0a58a0cf96e24c6ff9b5
github <kitakazetoshirokuma@gmail.com>
2021-04-19 21:13:58 +0900

    419-2

--- APG4b/CollectingBalls.cpp
+++ APG4b/CollectingBalls.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+//#include<algorithm>
 using namespace std;
 
 int main() {
@@ -23,3 +24,23 @@ int main() {
   cout << s << endl;
   return 0;
 }
+
+//別解
+// const int MAX_N = 100;
+// int N, K;
+// int X[MAX_N];
+
+// int main() {
+//   scanf("%d", &N);
+//   scanf("%d", &K);
+//   for(int i=0; i < N; i ++) {
+//     scanf("%d", X + i);
+//   }
+//   int ans = 0;
+//   for(int i =0; i < N; ++i) {
+//     int tmp = min(X[i], K - X[i]);
+//     ans += tmp;
+//   }
+//   printf("%d\n", ans * 2);
+//   return 0;
+// }
