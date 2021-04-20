3c1106ffc073cf78987d41aaf7d4f141384e7de5
github <kitakazetoshirokuma@gmail.com>
2021-04-20 22:27:05 +0900

    420-1a

--- APG4b/EX12.cpp
+++ APG4b/EX12.cpp
@@ -1,22 +1,38 @@
 #include<iostream>
 using namespace std;
 
+// int main() {
+//   string S;
+//   cin >> S;
+//   int sl = S.size();
+//   int i=0 ;
+//   int c=1;
+
+//   for(i=0; i < sl; i ++) {
+//     if(i % 2 == 0) continue;
+//     if(S.at(i) == '+') {
+//       c+=1;
+//     } else {
+//       c-=1;
+//     }
+//   }
+//   cout << c << endl;
+//   return 0;
+// }
+
+//参考 short
 int main() {
   string S;
   cin >> S;
-  int sl = S.size();
-  int i=0 ;
-  int c=1;
 
-  for(i=0; i < sl; i ++) {
-    if(i % 2 == 0) continue;
+  int ans = 1;
+  for(int i = 0; i < S.size(); i ++) {
     if(S.at(i) == '+') {
-      c+=1;
-    } else {
-      c-=1;
+      ans++;
     }
+    if(S.at(i) == '-') {
+      ans--;
+    } 
   }
-  cout << c << endl;
-  return 0;
-
+  cout << ans << endl;
 }
