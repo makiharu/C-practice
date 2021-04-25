5cfa53fa0ec4a2a17b5cc47a1813da67baa432d2
github <kitakazetoshirokuma@gmail.com>
2021-04-25 10:52:04 +0900

    425-2

---
+++ AOJ/SimpleCalculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  char op;
+  cin >> a >> op >> b;
+
+  if(op == '+') cout << a + b << endl;
+  if(op == '-') cout <<a - b << endl;
+  if(op == '*') cout <<a * b << endl;
+  if(op == '/') cout <<a / b << endl;
+  if(op == '?') return 0;
+}
