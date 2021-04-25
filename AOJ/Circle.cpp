95792a6dd85e4bf214fb0129d48d81d6cc2fef81
github <kitakazetoshirokuma@gmail.com>
2021-04-25 10:11:40 +0900

    425-1

---
+++ AOJ/Circle.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main() {
+  double r;
+  scanf("%lf", &r);
+  printf("%f %2f\n", r * r * M_PI, 2 * M_PI * r);
+  //cout << r * M_PI << endl;
+  return 0;
+}
