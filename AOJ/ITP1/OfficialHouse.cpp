c8d1067623204085d4e7c4d89c2622c5c2b22d28
github <kitakazetoshirokuma@gmail.com>
2021-05-24 07:33:20 +0900

    524-1

--- AOJ/ITP1/OfficialHouse.cpp
+++ AOJ/ITP1/OfficialHouse.cpp
@@ -2,33 +2,32 @@
 using namespace std;
 
 int main() {
-  int n, b, f, r, v;
-  int count[5][4][11];
+  int n,b,f,r,v;
+  int c[4][3][10];
 
-  for(int i = 1; i <= 4; i ++) {
-    for(int j = 1;j <= 3; j ++) {
-      for(int k = 1; k <= 10; k++) {
-        count[i][j][k] = 0;
+  for(int i=0;i<4;i++) {
+    for(int j=0;j<3;j++) {
+      for(int k=0;k<10;k++) {
+        c[i][j][k]=0;
       }
     }
   }
-
-  cin >> n;
-  for(int i =0; i < n; i++) {
-    int b, f, r, v;
-    cin >> b >> f >> r >> v;
-    count[b][f][r] += v;
-  } 
-
-  for(int p=1; p<=4; p ++) {
-    if(p > 1) cout << "####################\n";
-    for(int q =1; q <= 3; q++) {
-      for(int s = 1; s <= 10; s ++) {
-        cout << " " << count[p][q][s];
-      }
-      cout << endl;
-    }
+  cin>>n;
+  for(int i=0;i<n;i++) {
+    cin>>b>>f>>r>>v;
+    c[b][f][r]+=v;
   }
-  
+
+  for(int i=1;i<=4;i++) {
+    if(i>=1) cout << "####################" <<endl;
+    for(int j=0;j<=3;j++) {
+      for(int k=0;k<=10;k++) {
+        cout << " " << c[b][f][r];
+      }
+    }
+    cout << endl;
+  }
+
   return 0;
 }
+
