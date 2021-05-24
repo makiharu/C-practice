d25d4a335cf75d313f67b5afc091975137c2fc0c
github <kitakazetoshirokuma@gmail.com>
2021-05-24 21:44:48 +0900

    524-2

--- AOJ/ITP1/OfficialHouse.cpp
+++ AOJ/ITP1/OfficialHouse.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
 int main() {
   int n,b,f,r,v;
-  int c[4][3][10];
+  int c[5][4][11];
 
-  for(int i=0;i<4;i++) {
-    for(int j=0;j<3;j++) {
-      for(int k=0;k<10;k++) {
+  for(int i=1;i<=4;i++) {
+    for(int j=1;j<=3;j++) {
+      for(int k=1;k<=10;k++) {
         c[i][j][k]=0;
       }
     }
   }
+
   cin>>n;
   for(int i=0;i<n;i++) {
     cin>>b>>f>>r>>v;
     c[b][f][r]+=v;
   }
 
-  for(int i=1;i<=4;i++) {
-    if(i>=1) cout << "####################" <<endl;
-    for(int j=0;j<=3;j++) {
-      for(int k=0;k<=10;k++) {
-        cout << " " << c[b][f][r];
+  for(int p=1; p<=4; p ++) {
+    if(p > 1) cout << "####################\n";
+    for(int q =1; q <= 3; q++) {
+      for(int s = 1; s <= 10; s ++) {
+        cout << " " << c[p][q][s];
       }
+      cout << endl;
     }
-    cout << endl;
   }
-
   return 0;
 }
 
