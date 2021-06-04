db17ac67287c921c58da23f9c9381772be12647a
github <kitakazetoshirokuma@gmail.com>
2021-06-04 22:46:22 +0900

    0604

--- AOJ/ITP1/2A_SmallLarge.cpp
+++ AOJ/ITP1/2A_SmallLarge.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main() {
-  int a, b;
+  int a,b;
   cin >> a >> b;
-  if(a < b) {
-    cout << "a < b" << endl;
-  } else if(a > b) {
-    cout << "a > b" << endl;
-  } else {
-    cout << "a == b" << endl;
-  }
+
+  if(a < b) cout << "a < b" << endl;
+  else if(a > b) cout << "a > b" << endl;
+  else  cout << "a == b" << endl;
+
   return 0;
-}
+} 
