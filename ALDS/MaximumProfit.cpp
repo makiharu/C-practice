cbaca00e2d52821668aec5154ead8cdb9c781df9
github <kitakazetoshirokuma@gmail.com>
2021-06-30 21:57:07 +0900

    0630-1

--- ALDS/MaximumProfit.cpp
+++ ALDS/MaximumProfit.cpp
@@ -5,16 +5,15 @@ static const int MAX= 200000;
 int main() {
   int n, R[MAX];
   cin >> n;
-  for(int i = 0; i < n; i ++) {
-    cin >> R[i];
-  }
+  for(int i = 0; i < n; i ++) cin >> R[i];
+
   int maxv = -2000000000;
   int minv = R[0];
 
   for(int i = 1; i < n; i ++) {
     maxv = max(maxv, R[i] - minv);
-    minv = min(minv, R[i]);
+    minv = min(minv, R[0]);
   }
   cout << maxv << endl;
-  return 0;
+  return 0;  
 }
